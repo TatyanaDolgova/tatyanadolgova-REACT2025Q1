6cbccc9535eed0c3a1cd1a8c0ff373f68e48cc54
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2025-02-09 12:45:00 +0500

    feat: added tests

---
+++ src/hooks/useSearchRequest.test.tsx
@@ -0,0 +1,42 @@
+import { renderHook, act } from '@testing-library/react';
+import { useSearchRequest } from './useSearchRequest';
+import { beforeEach, describe, expect, it } from 'vitest';
+
+beforeEach(() => {
+  localStorage.clear();
+});
+
+describe('useSearchRequest', () => {
+  it('should initialize searchRequest from localStorage if present', () => {
+    localStorage.setItem('searchRequest', 'pikachu');
+
+    const { result } = renderHook(() => useSearchRequest());
+
+    expect(result.current[0]).toBe('pikachu');
+  });
+
+  it('should update searchRequest in localStorage when updateSearchRequest is called', () => {
+    const { result } = renderHook(() => useSearchRequest());
+
+    expect(result.current[0]).toBe(null);
+
+    act(() => {
+      result.current[1]('bulbasaur');
+    });
+
+    expect(result.current[0]).toBe('bulbasaur');
+
+    expect(localStorage.getItem('searchRequest')).toBe('bulbasaur');
+  });
+
+  it('should trim the query before saving to localStorage', () => {
+    const { result } = renderHook(() => useSearchRequest());
+
+    act(() => {
+      result.current[1]('  charizard  ');
+    });
+
+    expect(result.current[0]).toBe('charizard');
+    expect(localStorage.getItem('searchRequest')).toBe('charizard');
+  });
+});
--- src/hooks/useSearchRequest.tsx
+++ src/hooks/useSearchRequest.tsx
@@ -1,28 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 export const useSearchRequest = (): [
   string | null,
   (query: string) => void,
 ] => {
-  const [searchRequest, setSearchRequest] = useState<string | null>(null);
-
-  useEffect(() => {
-    const savedSearchRequest = localStorage.getItem('searchRequest');
-
-    if (savedSearchRequest) {
-      setSearchRequest(savedSearchRequest);
-    }
-  }, []);
+  const [searchRequest, setSearchRequest] = useState<string | null>(
+    localStorage.getItem('searchRequest')
+  );
 
   const updateSearchRequest = (query: string) => {
     const trimmedQuery = query.trim();
     setSearchRequest(trimmedQuery);
-    if (trimmedQuery) {
-      localStorage.setItem('searchRequest', trimmedQuery);
-    } else {
-      localStorage.removeItem('searchRequest');
-    }
+    localStorage.setItem('searchRequest', trimmedQuery);
   };
-
   return [searchRequest, updateSearchRequest];
 };
